e485ffaf4df2f0de2c6cc765cc267cf5c6d24175
ericlair <163988535+ericlair@users.noreply.github.com>
2026-09-11 22:50:13 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.08 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = [0] * len(numbers)
+    
+    for i in range(len(numbers)) :
+        answer[i] = numbers[i]*2
+    
+#-------------------------------------------    
+        
+    answer = []
+    
+    for i in range(len(numbers)) :
+        answer.append(numbers[i] * 2)
+    
+    return answer
